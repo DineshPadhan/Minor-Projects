8d0434f68391ce429dc45964112a930375710b9e
Dinesh <74955280+DineshPadhan@users.noreply.github.com>
2022-10-12 00:43:31 +0530

    day 8 update

--- 100Days100Project/Day_8 copy/app.js
+++ 100Days100Project/Day_8 copy/app.js
@@ -53,3 +53,36 @@ form.addEventListener('submit', (e) => {
 
 });
 
+
+//  google place suggection
+        
+// var searchInput = 'search_input';
+ 
+// $(document).ready(function () {
+//  var autocomplete;
+//  autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
+//   types: ['geocode'],
+//   /*componentRestrictions: {
+//    country: "USA"
+//   }*/
+//  });
+  
+//  google.maps.event.addListener(autocomplete, 'place_changed', function () {
+//   var near_place = autocomplete.getPlace();
+//  });
+// });     
+// var searchInput = 'search_input';
+ 
+//  $(document).ready(function () {
+//   var autocomplete;
+//   autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
+//    types: ['geocode'],
+//    /*componentRestrictions: {
+//     country: "USA"
+//    }*/
+//   });
+   
+//   google.maps.event.addListener(autocomplete, 'place_changed', function () {
+//    var near_place = autocomplete.getPlace();
+//   });
+//  });
