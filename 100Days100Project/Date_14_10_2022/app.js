9c5fdcf440f9eceaf428bdd6074f1caf4107bafc
Dinesh <74955280+DineshPadhan@users.noreply.github.com>
2022-11-01 21:37:37 +0530

    Date_01_11_2022 updates

--- 100Days100Project/Date_14_10_2022/app.js
+++ 100Days100Project/Date_14_10_2022/app.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", getjock);
 // button.addEventListener("click", getjock());
 
 async function getjock() {
-    const jokeData = await fetch("https://hindi-jokes-api.onrender.com/jokes?api_key=43e93baacbc58e0fc3749ba1593d",{
+    const jokeData = await fetch("https://hindi-jokes-api.onrender.com/jokes?api_key=72b23b7454683ef97ee637e3174f",{
         headers:{Accept:"application/json"},
     });
     const jokeObj = await jokeData.json();
