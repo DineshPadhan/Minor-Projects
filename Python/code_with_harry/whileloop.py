56f8fa75cfb44beea3983d32573bb808cd9ee247
Dinesh <74955280+DineshPadhan@users.noreply.github.com>
2023-03-07 21:28:51 +0530

    while loop

---
+++ Python/code_with_harry/whileloop.py
@@ -0,0 +1,18 @@
+# i = 1
+# while(i<=10):
+#     print(i)
+#     i+=1
+
+# i=10
+# while(i>=1):
+#     print(i)
+#     i-=1
+
+a= int(input("Enter your Score: "))
+while(a>=10):
+    print("Passed")
+    a= int(input("Enter your Score: "))
+else:
+    print("Failed")
+
+# In Python There are no do While loop but we can create
