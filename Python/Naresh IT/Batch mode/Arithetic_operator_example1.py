79129d43b974677063eb5a3f445c9f28d95f23c5
Dinesh <74955280+DineshPadhan@users.noreply.github.com>
2023-04-08 19:34:50 +0530

    python updates

---
+++ Python/Naresh IT/Batch mode/Arithetic_operator_example1.py
@@ -0,0 +1,12 @@
+# Demonstrate Arithmetic operator
+a=float(input("Enter a value: "))
+b=float(input("Enter a value: "))
+print("="*30)
+print("%f + %f = %f"%(a,b,a+b))
+print("%f - %f = %f"%(a,b,a-b))
+print("%f * %f = %f"%(a,b,a*b))
+print("%f / %f = %f"%(a,b,a/b))
+print("%f // %f = %f"%(a,b,a//b))
+print("%f & %f = %f"%(a,b,a%b))
+print("%f ** %f = %f"%(a,b,a**b))
+print("="*30)
---
+++ Python/Naresh IT/Batch mode/Arithmetic_Oprator_example2.py
@@ -0,0 +1,13 @@
+# Demonstrate Arithmetic operator
+# Arithmetic_Oprator_example2
+a=float(input("Enter a value: "))
+print("="*30)
+sqrt=a**(1/2)
+# sqrt=a**(0.5)
+print("sqrt(%f) = %f"%(a,sqrt))
+cubert=a**(1/3)
+print("cubert(%f) = %f"%(a,cubert))
+
+# w a p program to eveluate a**m/a**n
+# w a p program to eveluate a**m**m
+# w a p program to eveluate
